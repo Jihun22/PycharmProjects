3dfd33c3ef8004ba28c6eb478253ac5ea4f6435b
YANHJIHUN <ik5334@naver.com>
2021-04-20 20:24:06 +0900

    Numpy실습

---
+++ untitled1/machine/Numpy21.py
@@ -0,0 +1,11 @@
+import  numpy as np
+
+data1 = [1,2,3,4,5]
+data2 = np.array(data1)
+
+arr1 = np.array([1,2,3,4,5,6,7,8,9])
+arr2 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(type(data1))
+print(type(data2))
+print(arr1.shape)
+print(arr2.shape)
