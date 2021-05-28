38d6234d1bd2091b77e08fee8b95046de25a6e9c
YANHJIHUN <ik5334@naver.com>
2021-05-28 17:52:15 +0900

    3.4 영화리뷰 분류 : 이진 분류 예제

--- untitled1/keras0527.py
+++ untitled1/keras0527.py
@@ -1,7 +1,7 @@
 #케라스 3.4 영화 리뷰 분류 : 이진 분류 예제
 #IMDB 데이터셋 로드하기
-from  keras.datasets import imdb
-(train_data,train_labels), (test_data, test_labels) =imdb.load_data(num_words=10000)
+from keras.datasets import imdb
+(train_data , train_labels), (test_data ,test_labels)= imdb.load_data(num_words= 10000)
 
 print(train_data[0])
 print(train_labels[0])
@@ -10,15 +10,65 @@ print(max([max(sequence) for sequence in train_data]))
 # 정수 시퀀스 이진 행렬로 인코딩하기
 import numpy as np
 
-def vectorize_sequences(sequences, dimension = 10000):
+def vectorize_sequences(sequences, dimension=10000):
+    # 크기가 (len(sequences), dimension))이고 모든 원소가 0인 행렬을 만듭니다
     results = np.zeros((len(sequences), dimension))
-    for i , sequence in enumerate(sequences):
-        results[i,sequence] = 1.
-        return  results
-x_train = vectorize_sequences(train_data)
-x_test = vectorize_sequences(test_data)
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.  # results[i]에서 특정 인덱스의 위치를 1로 만듭니다
+    return results
 
+# 훈련 데이터를 벡터로 변환합니다
+x_train = vectorize_sequences(train_data)
+# 테스트 데이터를 벡터로 변환합니다
+x_test = vectorize_sequences(test_data)
 print(x_train[0])
-#레이블을 벡터로 바꿈
-x_train = np.asarray(train_labels).astype('float32')
-x_test = np.asarray(test_labels).astype('float32')
+# 레이블을 벡터로 바꿉니다
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+#신경망의 케라스 구현
+from keras import models
+from keras import layers
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+#모델 컴파일 하기
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+#옵티마이저 설정하기
+from keras import optimizers
+
+model.compile(optimizer=optimizers.RMSprop(lr=0.001),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+# 손실과 측정을 함수 객체로 지정하기
+from keras import losses
+from keras import metrics
+
+model.compile(optimizer=optimizers.RMSprop(lr=0.001),
+              loss=losses.binary_crossentropy,
+              metrics=[metrics.binary_accuracy])
+
+#훈련 검증
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+#모델 훈련하기
+model.compile(optimizer='rmsprop',
+              loss= 'binary_crossentropy',
+              metrics=['acc'])
+
+history  = model.fit(partial_x_train,
+                     partial_y_train,
+                     epochs=20,
+                     batch_size=512,
+                     validation_data=(x_val, y_val))
+
+
