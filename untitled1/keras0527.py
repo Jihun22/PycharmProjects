13a61fa02a861a04fafaf5d8a713bf04a921a772
YANHJIHUN <ik5334@naver.com>
2021-06-09 21:18:52 +0900

    3-7 훈련과 검증 손실 그래프

--- untitled1/keras0527.py
+++ untitled1/keras0527.py
@@ -84,8 +84,8 @@ val_loss = history_dict['val_loss']
 
 epochs = range (1, len(loss) +1)
 
-plt.plot(epochs , loss , 'bo', label = 'Training loss')
-plt.plot(epochs, val_loss, 'b', label= 'Validation loss')
+plt.plot(epochs , loss , 'bo', label = 'Training loss') #'bo'는 파란색 점을 의미
+plt.plot(epochs, val_loss, 'b', label= 'Validation loss') #'b'는 파란색 실선을 의미
 plt.title('Training and validation loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
