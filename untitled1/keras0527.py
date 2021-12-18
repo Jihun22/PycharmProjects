3d4b6fbbe1907da42a7e715ddb304d7497952682
YANHJIHUN <ik5334@naver.com>
2021-12-18 21:52:31 +0900

    케라스 모델 처음부터 훈련 , 훈련된 새로운 데이터 예측

--- untitled1/keras0527.py
+++ untitled1/keras0527.py
@@ -106,3 +106,19 @@ plt.ylabel('Accuracy')
 plt.legend()
 
 plt.show()
+
+#3-11 모델 처음부터 다시 훈련 2021-12-18
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=4, batch_size=512)
+results = model.evaluate(x_test, y_test)
+print(results)
+# 3.4.5 훈련된 모델로 새로운 데이터 예측
+print(model.predict(x_train))
