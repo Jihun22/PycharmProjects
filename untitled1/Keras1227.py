f655fb4415cdf589d57fce8e51634b802df25664
YANHJIHUN <ik5334@naver.com>
2021-12-27 21:36:27 +0900

    K-겹 검증을 사용한 훈련 검증

--- untitled1/Keras1227.py
+++ untitled1/Keras1227.py
@@ -29,3 +29,37 @@ def build_model():
     model.add(layers.Dense(1))
     model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
     return  model
+
+# K-겹 검증을 사용한 훈련검증
+import numpy as np
+
+k = 4
+num_val_samples = len(train_data) // k
+num_epochs =100
+all_scores = []
+for i in range(k):
+    print('처리중인 폴드 #' , i)
+    val_data =train_data[i * num_val_samples : (i + 1) * num_val_samples]
+    val_targets = train_targets[i * num_val_samples: (i +1) * num_val_samples]
+
+    #훈련 데이터 준비 : 다른 분할 전체
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis =0)
+    partial_train_targets = np.concatenate(
+        [train_targets[:i * num_val_samples],
+         train_targets[(i + 1) * num_val_samples:]],
+        axis = 0)
+
+    # 케라스 모델 구성 (컴파일 포함)
+    model = build_model()
+    # 모델 훈련(verbose=0 이므로 훈련 과정이 출력되지 않습니다)
+    model.fit(partial_train_data, partial_train_targets,
+              epochs=num_epochs , batch_size=1, verbose =0)
+    # 검증 세트로 모델 평가
+    val_mse, val_mae = model.evaluate(val_data, val_targets, verbose =0)
+    all_scores.append(val_mae)
+print('all_scores:',all_scores)
+
+print('np.mean:',np.mean(all_scores))
