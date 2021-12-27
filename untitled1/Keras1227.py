4b9ca3d361bd61305c63feea1bd872dd06e5f340
YANHJIHUN <ik5334@naver.com>
2021-12-27 21:05:24 +0900

    데이터 정규화

--- untitled1/Keras1227.py
+++ untitled1/Keras1227.py
@@ -5,3 +5,12 @@ print(train_data.shape)
 print(test_data.shape)
 print(train_targets)
 
+#데이터 준비
+#데이터 정규화
+mean = train_data.mean(axis=0)
+train_data -= mean
+std = train_data.std(axis=0)
+train_data /= std
+
+test_data -= mean
+test_data /= std
