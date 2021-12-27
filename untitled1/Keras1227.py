6d175c149a3f5a82bbf39736305f99f96e4baae7
YANHJIHUN <ik5334@naver.com>
2021-12-27 22:23:23 +0900

    각 폴드에 검증 점수를 로그에 저장

--- untitled1/Keras1227.py
+++ untitled1/Keras1227.py
@@ -72,20 +72,25 @@ num_epochs = 500
 all_mae_histories = []
 for i in range(k):
     print('처리중인 폴드 #', i)
-    # 검증 데이터 준비 :k 번째 분할
-    val_data = train_data[i * num_val_samples: (i +1) * num_val_samples]
+    # 검증 데이터 준비: k번째 분할
+    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
     val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
 
-    #훈련 데이터 준비 : 다른 분할 전체
-    partial_train_data = np.concatenate (
+    # 훈련 데이터 준비: 다른 분할 전체
+    partial_train_data = np.concatenate(
+        [train_data[:i * num_val_samples],
+         train_data[(i + 1) * num_val_samples:]],
+        axis=0)
+    partial_train_targets = np.concatenate(
         [train_targets[:i * num_val_samples],
          train_targets[(i + 1) * num_val_samples:]],
-        axis= 0)
+        axis=0)
+
     # 케라스 모델 구성(컴파일 포함)
     model = build_model()
-    # 모델 훈련 (verbose=0 이므로 훈련과정이 출력 되지 않습니다.)
+    # 모델 훈련(verbose=0 이므로 훈련 과정이 출력되지 않습니다)
     history = model.fit(partial_train_data, partial_train_targets,
                         validation_data=(val_data, val_targets),
                         epochs=num_epochs, batch_size=1, verbose=0)
-    mae_history = history.histroy['val_mean_absolute_error']
+    mae_history = history.history['val_mean_absolute_error']
     all_mae_histories.append(mae_history)
