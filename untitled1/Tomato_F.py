efabba94306480dbc400c0ac2d423b16d223fbae
YANHJIHUN <ik5334@naver.com>
2022-01-02 20:11:28 +0900

    생장길이와 열매수 관계 완성

--- untitled1/Tomato_F.py
+++ untitled1/Tomato_F.py
@@ -12,24 +12,38 @@ df1 =pd.read_csv(file_path)
 #print(df1)
 #해당 열 출력 print(df1[["주차","생장길이","열매수"]])
 #데이터 가공
-df1["생장길이"] /=200
-df1["열매수"] /=100
-
+df2=df1['생장길이']
+df3=df1['열매수']
+df4=df1['주차']
+#print(df2)
+#print(df3)
 week_class= {
-    "4주차": [1,0,0],
-    "9주차": [0,1,0],
-    "18주차":[0,0,1]
+    "4주차": [15,14],
+    "9주차": [22,13],
+    "18주차":[15,17]
 }
-
-y= np.empty((20000,3))
+#print(week_class)
+tuples= df2
+tupp=df3
+tupleso = pd.Series(df3)
+tupleson = pd.Series(df2)
+print(tupleso)
+Tomato_df = pd.DataFrame({'열매수':df3,
+                          '생장길이':df2,
+                          '주차':df4})
+#생장길이와 열매수 관계
+Tomato_df['평균값'] = (df2 /df3)
+print(Tomato_df)
+'''''
+y= np.empty((200,3))
 for i,v in enumerate(df1["주차"]):
     y[i] = week_class[v]
 print(y[0:3])
 
 x= df1[["생장길이", "열매수"]].values
 
-x_train, y_train = x[1:15001] , y[1:15001]
-x_test, y_test = x[15001:20001] , y[150001,20001]
+x_train, y_train = x[1:100] , y[1:100]
+x_test, y_test = x[2:200] , y[2,200]
 
 #모델 생성
 model =Sequential()
@@ -53,3 +67,4 @@ model.fit(x_train,y_train)
 #정답률
 score= model.evaluate(x_test,y_test)
 print("score:",score)
+'''
