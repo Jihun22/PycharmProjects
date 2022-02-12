73d7e6504ae483766a7c9f6786c4d4a695d26c3a
YANHJIHUN <ik5334@naver.com>
2022-02-13 02:12:50 +0900

    vgg19 모델 세팅

--- untitled1/TomatoG19.py
+++ untitled1/TomatoG19.py
@@ -7,13 +7,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from glob import glob
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Model
 from tensorflow.keras.applications.inception_v3 import InceptionV3
-from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
 from tensorflow.keras.applications.vgg19 import VGG19
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
 #plt 한글 폰트
 matplotlib.rcParams['font.family'] ='Malgun Gothic'
 
@@ -21,12 +20,18 @@ matplotlib.rcParams['axes.unicode_minus'] =False
 #plt 한글폰트 끝
 
 #데이터셋 위치
+path = '/Users/ik533/Desktop/archive/New Plant Diseases Dataset(Augmented)'
+os.listdir(path)
+print (path)
+
+train_path = os.path.join(path, "train")
+print(os.listdir(train_path))
+print("*"*100)
+test_path = os.path.join(path, "valid")
+print(os.listdir(test_path))
+
 folders = glob('/Users/ik533/Desktop/archive/New Plant Diseases Dataset(Augmented)/train/*')
-
-print (folders)
-
-
-SIZE = [128, 128]
+print(folders)
 
 plt.imshow(plt.imread("/Users/ik533/Desktop/archive/New Plant Diseases Dataset(Augmented)/train/Tomato___Bacterial_spot/00416648-be6e-4bd4-bc8d-82f43f8a7240___GCREC_Bact.Sp 3110.JPG"))
 plt.title("세균반점")
@@ -39,3 +44,10 @@ plt.show()
 plt.imshow(plt.imread("/Users/ik533/Desktop/archive/New Plant Diseases Dataset(Augmented)/train/Tomato___Late_blight/0003faa8-4b27-4c65-bf42-6d9e352ca1a5___RS_Late.B 4946.JPG"))
 plt.title("감자역병균")
 plt.show()
+SIZE = [128, 128]
+vg19 = VGG19(input_shape=SIZE + [3], weights="imagenet", include_top=False)
+
+for layer in vg19.layers:
+    layer.trainable = False
+
+x =Flatten()(vg19.output)
