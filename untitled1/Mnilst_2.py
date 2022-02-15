1205ad8aad87dc330474e67f090967f3de138a85
YANHJIHUN <ik5334@naver.com>
2022-02-15 21:53:04 +0900

    텐서플로 공홈 튜토리얼로 변경 기존 오류 해결

--- untitled1/Mnilst_2.py
+++ untitled1/Mnilst_2.py
@@ -1,27 +1,23 @@
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-#mnist 데이터셋 다운로드
-mnist = tf.keras.datasets.mnist
+
+# 데이터셋 로드
+mnist =tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
-print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
-#시각화
+#레이어 쌓기
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
 
-import matplotlib.pyplot as plt
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
-plt.figure(figsize=(10, 10))
-c = 0
-for x in range(5):
-    for y in range(3):
-        plt.subplot(5, 3, c + 1)
-        plt.imshow(x_train[c], cmap='gray')
-        c += 1
+model.fit(x_train, y_train, epochs=5)
 
-plt.show()
-
-print(y_train[:15])
-
-# 모델 Sequential 모델 생성
+model.evaluate(x_test,  y_test, verbose=2)
