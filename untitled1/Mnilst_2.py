b9b52154d61b1902dc300e4c4722af35c82eaa6c
YANHJIHUN <ik5334@naver.com>
2022-02-15 21:54:27 +0900

    코드 설명 추가

--- untitled1/Mnilst_2.py
+++ untitled1/Mnilst_2.py
@@ -3,10 +3,11 @@ import tensorflow as tf
 # 데이터셋 로드
 mnist =tf.keras.datasets.mnist
 
+# 샘플  값을 정수에서 보동소수로 변환
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-#레이어 쌓기
+#층을 차례대로 쌓아 tf.keras.Sequential 모델을 만듬. 훈련에 사용할 옵티마이저(optimizer)와 손실 함수를 선택
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(128, activation='relu'),
@@ -17,7 +18,7 @@ model = tf.keras.models.Sequential([
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-
+#  모델 훈련 하고 평가
 model.fit(x_train, y_train, epochs=5)
 
 model.evaluate(x_test,  y_test, verbose=2)
