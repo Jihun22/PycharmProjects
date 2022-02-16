33f3ae26b0ce93696273059f6625120ae20598f8
YANHJIHUN <ik5334@naver.com>
2022-02-16 23:04:38 +0900

    데이터 전처리

--- untitled1/TensprFlow_im.py
+++ untitled1/TensprFlow_im.py
@@ -8,3 +8,20 @@ import matplotlib.pyplot as plt
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images , train_labels) , (test_images, test_labels) = fashion_mnist.load_data()
 
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape)
+
+print(len(train_labels))
+print(train_labels)
+
+print(test_images.shape)
+print(len(test_labels))
+
+#데이터 전처리
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
