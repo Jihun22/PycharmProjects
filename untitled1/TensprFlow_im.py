f390b65d38d0b37695a1b6251eae9a605ef95083
YANHJIHUN <ik5334@naver.com>
2022-02-19 21:03:24 +0900

    코드 수정  show 안나오는 증상

--- untitled1/TensprFlow_im.py
+++ untitled1/TensprFlow_im.py
@@ -75,39 +75,53 @@ probability_model = tf.keras.Sequential([model,
 predictions = probability_model.predict(test_images)
 
 print(predictions[0])
-np.argmax(predictions[0])
+print(np.argmax(predictions[0]))
 
 #10개의 클래스에 대한 예측을 모두 그래프로 표현
 
-def plot_image(i, predictions_array , true_label, img):
-    true_label, img = true_label[i], img[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
 
-    plt.imshow(img, cmap = plt.cm.binary)
+  plt.imshow(img, cmap=plt.cm.binary)
 
-    predicted_label = np.argmax(predictions_array)
-    if predicted_label == true_label:
-        color ='blue'
-    else:
-        color = 'red'
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
 
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                         100*np.max(predictions_array),
-                                         class_names[true_label]),
-                                         color =color)
-    def plot_value_array(i, predictions_array , true_label):
-        true_label = true_label[i]
-        plt.grid(False)
-        plt.xticks(range(10))
-        plt.yticks([])
-        thisplot = plt.bar(range(10), predictions_array , color ="#777777")
-        plt.ylim([0,1])
-        predicted_label = np.argmax(predictions_array)
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
 
-        thisplot[predicted_label].set_color('red')
-        thisplot[true_label].set_clor('blue')
+def plot_value_array(i, predictions_array, true_label):
+  true_label = true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
 
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+#예측 확인
+i = 0
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions[i], test_labels)
+plt.show()
 
-
+i = 12
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions[i], test_labels)
+plt.show()
