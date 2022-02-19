7f9218a7216b6d8ade184470ab2a440910b04f7d
YANHJIHUN <ik5334@naver.com>
2022-02-20 02:50:36 +0900

    네트워크 구성

--- untitled1/Tomato0212.py
+++ untitled1/Tomato0212.py
@@ -1,9 +1,15 @@
+import matplotlib
 import numpy as np
+from keras import models
 from tensorflow import keras
 from tensorflow.keras import layers
 import os
-from glob import glob
-# Model / data parameters
+import matplotlib.pyplot as plt
+#plt 한글 폰트
+matplotlib.rcParams['font.family'] ='Malgun Gothic'
+
+matplotlib.rcParams['axes.unicode_minus'] =False
+#plt 한글폰트 끝
 
 #데이터 경로
 
@@ -19,5 +25,32 @@ print("*"*100)
 test_path = os.path.join(path, "valid")
 print(os.listdir(test_path))
 
+#그림 시각화
+plt.imshow(plt.imread("/Users/ik533/Desktop/archive/New Plant Diseases Dataset(Augmented)/train/Tomato___Bacterial_spot/00416648-be6e-4bd4-bc8d-82f43f8a7240___GCREC_Bact.Sp 3110.JPG"))
+plt.title("세균반점")
+plt.show()
 
+plt.imshow(plt.imread("/Users/ik533/Desktop/archive/New Plant Diseases Dataset(Augmented)/train/Tomato___Early_blight/0034a551-9512-44e5-ba6c-827f85ecc688___RS_Erly.B 9432.JPG"))
+plt.title("겹무늬병")
+plt.show()
 
+plt.imshow(plt.imread("/Users/ik533/Desktop/archive/New Plant Diseases Dataset(Augmented)/train/Tomato___Late_blight/0003faa8-4b27-4c65-bf42-6d9e352ca1a5___RS_Late.B 4946.JPG"))
+plt.title("감자역병균")
+plt.show()
+
+#네트워크 구성
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                        input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.summary()
