cfc322c9e71350398b290515b96aa4e865acd01c
YANHJIHUN <ik5334@naver.com>
2022-02-20 14:12:00 +0900

    그래프 작성

--- untitled1/Tomato0212.py
+++ untitled1/Tomato0212.py
@@ -91,10 +91,32 @@ for data_batch, labels_batch in train_generator:
     print('배치 레이블 크기:', labels_batch.shape)
     break
 
-histor = model.fit_generator(
+history = model.fit_generator(
     train_generator,
     steps_per_epoch=100,
     epochs=30,
     validation_data=validation_generator,
     validation_steps=50)
 
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
